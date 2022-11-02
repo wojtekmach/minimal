15e7c3499f4d84c16f37d37ca140ffcdc4967215
Wojtek Mach <wojtek@wojtekmach.pl>
2022-11-02 21:34:09 +0100

    up

--- swiftui/Main.swift
+++ swiftui/Main.swift
@@ -8,3 +8,16 @@ struct Main: App {
         }
     }
 }
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .frame(minWidth: 400, minHeight: 400)
+        .padding()
+    }
+}
